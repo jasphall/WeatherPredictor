035684f6718cf29d60396c0cad533a405c0b9851
Maciej Sikorski <maciekksikorski@gmail.com>
2017-07-11 19:27:33 +0200

    Pliki projektu

---
+++ src/main/java/com/sikorski/weatherapp/predictor/WeatherPredictorApplication.java
@@ -0,0 +1,12 @@
+package com.sikorski.weatherapp.predictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherPredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherPredictorApplication.class, args);
+	}
+}
